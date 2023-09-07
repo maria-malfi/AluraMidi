c03be7cc3e7685c170c9666f1ec3d628174ca20f
maria-malfi <128652455+maria-malfi@users.noreply.github.com>
2023-09-06 22:24:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-document.querySelector('.tecla_pom')
+function tocaSom (selectorAudio){
+  const elemento = document.querySelector(selectorAudio).play();
+
+  if (elementos && elemento.localName === 'audio'){
+      elemento.play();
+   }
+  else{ 
+      //alert('Elemento não encontrado ou seletor inválido'):
+      console.log(Elemento ou seletor não encontrado!)
+
+  }
+}
+
+const listadeTeclas = document.querySelector('.tecla');
+
+
+for (let contador = 0; contador < listadeTeclas.length; contador++ ){
+    
+}
